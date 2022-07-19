e1081daf85987db0147d2bf9d0586ca1fae2a125
Arthur Chang <s9001055@gmail.com>
2022-07-20 01:38:08 +0800

    new repo

---
+++ src/hashset.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include<iostream>
+#include<vector>
+
+class MyHashSet {
+public:
+    MyHashSet() {
+        
+    }
+    
+    void add(int key) {
+        
+    }
+    
+    void remove(int key) {
+        
+    }
+    
+    bool contains(int key) {
+        
+    }
+};
